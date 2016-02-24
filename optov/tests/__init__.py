1fe9ce0e6135060a179c77ab3c7d9a4cefafa5b9
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2016-02-24 12:02:12 +0100

    initial optov project structure commit

---
+++ optov/tests/__init__.py
@@ -0,0 +1,10 @@
+'''
+optov: tests module.
+
+Meant for use with py.test.
+Organize tests into files, each named xxx_test.py
+Read more here: http://pytest.org/
+
+Copyright 2016, Malte Aschermann
+Licensed under LGPL
+'''
---
+++ optov/tests/basic_test.py
@@ -0,0 +1,13 @@
+'''
+optov: Test module.
+
+Meant for use with py.test.
+Write each test as a function named test_<something>.
+Read more here: http://pytest.org/
+
+Copyright 2016, Malte Aschermann
+Licensed under LGPL
+'''
+
+def test_example():
+    assert True
