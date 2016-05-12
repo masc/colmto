cddbb52342769e11f202116659a9c27f844e140c
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2016-05-12 15:06:26 +0200

    split timeloss and traveltime calculation

--- optov/common/statistics.py
+++ optov/common/statistics.py
@@ -36,6 +36,30 @@ class Statistics(object):
                           "worst" : [],
                           "random": []
                           }
+
+        l_runs = 0
+        l_vehicles = 0
+        for i_sortingmode, i_scenarioruns in p_scenarioruns.iteritems():
+            l_runs = len(i_scenarioruns)
+            for i_run, i_scenariorun in i_scenarioruns.iteritems():
+                l_ettripinfotree = ElementTree.parse(i_scenariorun.get("tripinfofile"))
+                l_ettripinfos = l_ettripinfotree.getroot()
+                l_vehicles = len(l_ettripinfos)
+                for i_tripinfo in l_ettripinfos:
+                    l_traveltimes.get(i_sortingmode).append(float(i_tripinfo.get("duration")))
+
+
+        self._visualisation.boxplot("Traveltime-{}_{}_vehicles_{}runs_one21segment.{}".format(p_scenarioname, l_vehicles, l_runs, "pdf"),
+                                    l_traveltimes,
+                                    "{}: Travel time for \n{} vehicles, {} runs for each mode, one 2+1 segment".format(p_scenarioname, l_vehicles, l_runs),
+                                    "configuration modes (initial sorting)",
+                                    "traveltime in seconds"
+                                    )
+
+
+    def timeloss(self, p_scenarioname, p_scenarioruns):
+        print("* time loss statistics for scenario {}".format(p_scenarioname))
+
         l_timeloss = { "best" : [],
                        "worst" : [],
                        "random": []
@@ -50,16 +74,7 @@ class Statistics(object):
                 l_ettripinfos = l_ettripinfotree.getroot()
                 l_vehicles = len(l_ettripinfos)
                 for i_tripinfo in l_ettripinfos:
-                    l_traveltimes.get(i_sortingmode).append(float(i_tripinfo.get("duration")))
                     l_timeloss.get(i_sortingmode).append(float(i_tripinfo.get("timeLoss")))
-
-
-        self._visualisation.boxplot("Traveltime-{}_{}_vehicles_{}runs_one21segment.{}".format(p_scenarioname, l_vehicles, l_runs, "pdf"),
-                                    l_traveltimes,
-                                    "{}: Travel time for \n{} vehicles, {} runs for each mode, one 2+1 segment".format(p_scenarioname, l_vehicles, l_runs),
-                                    "configuration modes (initial sorting)",
-                                    "traveltime in seconds"
-                                    )
 
         self._visualisation.boxplot("TimeLoss-{}_{}_vehicles_{}runs_one21segment.{}".format(p_scenarioname, l_vehicles, l_runs, "pdf"),
                                     l_timeloss,
@@ -67,5 +82,3 @@ class Statistics(object):
                                     "configuration modes (initial sorting)",
                                     "time loss in seconds"
                                     )
-
-
