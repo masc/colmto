33458583dee12882d6257139f999447bf869348d
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2016-10-05 14:28:26 +0200

    fixed detector order

--- optom/common/statistics.py
+++ optom/common/statistics.py
@@ -134,7 +134,7 @@ class Statistics(object):
         l_root = etree.parse(l_iloopfile)
         l_iloop_detections = etree.XSLT(s_iloop_template)(l_root).iter("vehicle")
 
-        l_detectors = p_scenario_config.get("detectorpositions").keys()
+        l_detectors = sorted(p_scenario_config.get("detectorpositions").keys())
 
         # create a dictionary with vid -> detectorid -> timestep hierarchy for json output, for csv the same but flat
         l_vehicle_data_json = defaultdict(dict)
@@ -156,9 +156,8 @@ class Statistics(object):
             i_vdata["vtype"] = l_vehicles.get(i_vid).vtype
             i_vdata["speed_max"] = l_vehicle_speed_max
 
-            l_detector_pairs = zip(l_detectors[:-1], l_detectors[1:])
-            #[(l_detectors[0], l_detectors[-1])] + \
-                               #[(l_detectors[1], l_detectors[-2])] +
+            l_detector_pairs = [(l_detectors[0], l_detectors[-1]), (l_detectors[1], l_detectors[-2])] + zip(l_detectors[1:-2:2], l_detectors[2:-1:2])
+
             for i_detector_x, i_detector_y in l_detector_pairs:
                 l_traveltime = None
                 l_opt_travel_time = None
