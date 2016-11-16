c1127d193e139a25a3ea35ca027613f56bc6df49
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2016-11-16 12:48:38 +0100

    added radon script

---
+++ radon.sh
@@ -0,0 +1,21 @@
+echo "# Radon\n"
+echo "## Cyclomatic Complexity\n"
+echo "| File | Type | R:C | Module | CC  |\n|:---- |:---- |:---:|:------ |:--- |"
+radon cc --show-closures -x F --total-average -s optom | sed \
+-e 's/^    /||/g' \
+-e '/^||/ s/ - /|/g' \
+-e '/^||/ s/ /|/g' \
+-e '/^||/ s/|(/ (/g' \
+-e '/^||/ s/$/|/' \
+-e '/^[a-zA-Z]/ s/^/|`/' \
+-e '/^|`[a-zA-Z]/ s/$/`|/' \
+-e '/^||/ s/|[[:alnum:]\.\_]\{2\}[[:alnum:]\.\_]*|/|`&`|/g' \
+-e '/^||/ s/|`|/|`/' \
+-e '/^||/ s/|`|/`|/' \
+-e '/|`Average[[:print:]]*`|/ s/|//g'
+echo "\n## Maintainability Index\n"
+echo "| Module | MI  |\n|:------ |:--- |"
+radon mi -x F -s optom | sed \
+-e 's/ - /`|/g' \
+-e '/^[a-zA-Z]/ s/^/|`/' \
+-e '/^|`[a-zA-Z]/ s/$/|/'
