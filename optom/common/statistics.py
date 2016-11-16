a4c854bf6ba31ff1919640e132e086e8abad9c69
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2016-11-16 16:33:53 +0100

    docu, removed unused code

--- optom/common/statistics.py
+++ optom/common/statistics.py
@@ -80,63 +80,6 @@ class Statistics(object):
         self._log = log.logger(__name__, p_args.loglevel, p_args.logfile)
         self._writer = Writer(p_args)
 
-    def compute_sumo_results(self, p_scenarioname, p_scenarioruns, p_iloopresults,
-                             p_deltas=("1_pre21-2_post21",
-                                       "1_pre21-3_exit",
-                                       "2_post21-3_exit")):
-        """Compute travel time statistics for given scenario"""
-
-        self._log.info("Travel time statistics for scenario %s", p_scenarioname)
-
-        l_data = {
-            "traveltime": {},
-            "timeloss": {},
-            "relativetimeloss": {}
-        }
-
-        l_runs = 0
-        l_vehicles = 0
-
-        for i_sortingmode, i_scenarioruns in p_scenarioruns.get("runs").iteritems():
-            l_runs = len(i_scenarioruns)
-
-            for i_run, i_scenariorun in i_scenarioruns.iteritems():
-                l_tripfname = i_scenariorun.get("tripfile")
-                l_ettripstree = etree.parse(l_tripfname)
-                l_ettrips = l_ettripstree.getroot()
-                l_trips = dict(
-                    map(lambda t: (t.attrib.get("id"), t.attrib), l_ettrips.iter("vType")))
-
-                if l_vehicles == 0:
-                    l_vehicles = len(p_iloopresults.get(i_sortingmode).get(i_run))
-
-                for i_vid in l_trips:
-                    l_vmaxspeed = l_trips.get(i_vid).get("maxSpeed")
-                    l_label = "{}\n({} m/s)".format(i_sortingmode,
-                                                    str(int(float(l_vmaxspeed))).zfill(2))
-
-                    for i_delta in p_deltas:
-                        if l_data.get("traveltime").get(i_delta) is None:
-                            l_data.get("traveltime")[i_delta] = {}
-                        if l_data.get("traveltime").get(i_delta).get(l_label) is None:
-                            l_data.get("traveltime").get(i_delta)[l_label] = []
-                        l_data.get("traveltime").get(i_delta).get(l_label).append(
-                            p_iloopresults.get(i_sortingmode).get(i_run).get(i_vid).get(i_delta)
-                        )
-                        if l_data.get("timeloss").get(i_delta) is None:
-                            l_data.get("timeloss")[i_delta] = {}
-                        if l_data.get("timeloss").get(i_delta).get(l_label) is None:
-                            l_data.get("timeloss").get(i_delta)[l_label] = []
-                        l_data.get("timeloss").get(i_delta).get(l_label).append(
-                            p_iloopresults.get(i_sortingmode).get(i_run).get(i_vid).get(i_delta)
-                        )
-
-        return {
-            "data": l_data,
-            "nbvehicles": l_vehicles,
-            "nbruns": l_runs
-        }
-
     def dump_traveltimes_from_iloops(self, p_run_data, p_run_config, p_scenario_config,
                                      p_scenarioname, p_initialsorting, p_current_run, p_resultsdir):
         """Reading and aggregating induction loop logs and write them to csv/json files."""
@@ -206,7 +149,7 @@ class Statistics(object):
 
             l_vehicle_data_csv.append(l_vehicle_data_csv_row)
 
-        self._log.debug("Writing {} results".format(p_scenarioname))
+        self._log.debug("Writing %s results", p_scenarioname)
         l_aadtveh = "{}aadt".format(p_scenario_config.get("parameters").get("aadt")) \
             if p_run_config.get("aadt").get("enabled") \
             else "{}veh".format(p_run_config.get("nbvehicles").get("value"))
