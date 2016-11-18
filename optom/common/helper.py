c9b07eab4a0c46b0ce790ee0a68406bcebf430ed
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2016-11-18 12:19:53 +0100

    helper module: disable=too-few-public-methods

--- optom/common/helper.py
+++ optom/common/helper.py
@@ -9,6 +9,7 @@ class Enum(tuple):
 
 class Namespace(object):
     """Namespace similar to argparse"""
+    # pylint: disable=too-few-public-methods
     def __init__(self, **kwargs):
         """C'tor."""
         self.__dict__.update(kwargs)
