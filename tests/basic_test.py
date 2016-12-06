b1065fdde95bcc09a5ce5ce90e32a017457e9e06
Malte Aschermann <masc@tu-clausthal.de>
2016-12-06 14:40:43 +0100

    basic tests

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,13 +1,7 @@
-'''
-optov: Test module.
+"""
+optom: Basic test module.
+"""
 
-Meant for use with py.test.
-Write each test as a function named test_<something>.
-Read more here: http://pytest.org/
-
-Copyright 2016, Malte Aschermann
-Licensed under LGPL
-'''
 
 def test_example():
     assert True
