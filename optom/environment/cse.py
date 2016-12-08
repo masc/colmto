b77b8018226e785739728d877db1cdb4ed0e6e42
Malte Aschermann <masc@tu-clausthal.de>
2016-12-08 18:05:29 +0100

    added CSE class skeleton

---
+++ optom/environment/cse.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @package optom
+# @cond LICENSE
+# #############################################################################
+# # LGPL License                                                              #
+# #                                                                           #
+# # This file is part of the Optimisation of Overtaking Manoeuvres project.   #
+# # Copyright (c) 2016, Malte Aschermann (malte.aschermann@tu-clausthal.de)   #
+# # This program is free software: you can redistribute it and/or modify      #
+# # it under the terms of the GNU Lesser General Public License as            #
+# # published by the Free Software Foundation, either version 3 of the        #
+# # License, or (at your option) any later version.                           #
+# #                                                                           #
+# # This program is distributed in the hope that it will be useful,           #
+# # but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+# # GNU Lesser General Public License for more details.                       #
+# #                                                                           #
+# # You should have received a copy of the GNU Lesser General Public License  #
+# # along with this program. If not, see http://www.gnu.org/licenses/         #
+# #############################################################################
+# @endcond
+"""CSE classes"""
+
+
+class BaseCSE(object):
+    """Base class for the central optimisation entity (CSE)."""
+
+
+class SumoCSE(BaseCSE):
+    """SUMO related class for the central optimisation entity (CSE)."""
