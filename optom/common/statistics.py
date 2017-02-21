d99c0a9520fb330d2a591419408024c6b36e6808
Malte Aschermann <masc@tu-clausthal.de>
2017-02-21 20:24:31 +0100

    updated license

--- optom/common/statistics.py
+++ optom/common/statistics.py
@@ -5,7 +5,7 @@
 # # LGPL License                                                              #
 # #                                                                           #
 # # This file is part of the Optimisation of Overtaking Manoeuvres project.   #
-# # Copyright (c) 2016, Malte Aschermann (malte.aschermann@tu-clausthal.de)   #
+# # Copyright (c) 2017, Malte Aschermann (malte.aschermann@tu-clausthal.de)   #
 # # This program is free software: you can redistribute it and/or modify      #
 # # it under the terms of the GNU Lesser General Public License as            #
 # # published by the Free Software Foundation, either version 3 of the        #
@@ -22,6 +22,7 @@
 # @endcond
 """Statistics module"""
 from __future__ import division
+
 
 import optom.common.io
 import optom.common.log
