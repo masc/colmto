0887aa9b7b4d26bd9ebb17551d77a2bcd4824d2f
Malte Aschermann <masc@tu-clausthal.de>
2017-03-03 13:34:47 +0100

    fixed file discriptor leakage

--- optom/common/log.py
+++ optom/common/log.py
@@ -54,27 +54,41 @@ def logger(name, loglevel=logging.NOTSET, quiet=False,
 
     l_log.setLevel(l_level if l_level is not None else logging.NOTSET)
 
-    # create a file handler
-    l_fhandler = logging.handlers.RotatingFileHandler(
-        logfile, maxBytes=100 * 1024 * 1024, backupCount=16
-    )
-    l_fhandler.setLevel(l_level if l_level is not None else logging.NOTSET)
-
     # create a logging format
     l_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    l_fhandler.setFormatter(l_formatter)
 
-    # add the handlers to the logger
-    l_log.addHandler(l_fhandler)
+    # create a file/stream handler if not already done
+    l_add_fhandler = True
+    l_add_qhandler = True
+    for i_handler in l_log.handlers:
+        if isinstance(i_handler, logging.handlers.RotatingFileHandler):
+            i_handler.setLevel(l_level if l_level is not None else logging.NOTSET)
+            i_handler.setFormatter(l_formatter)
+            l_add_fhandler = False
+        if isinstance(i_handler, logging.StreamHandler):
+            i_handler.setLevel(l_level)
+            i_handler.setFormatter(l_formatter)
+            l_add_qhandler = False
 
-    # create a stdout handler if not set to quiet
-    if not isinstance(quiet, bool):
-        raise KeyError("quiet ({}) is {}, but bool expected.".format(quiet, type(quiet)))
+    if l_add_fhandler:
+        l_fhandler = logging.handlers.RotatingFileHandler(
+            logfile, maxBytes=100 * 1024 * 1024, backupCount=16
+        )
+        l_fhandler.setLevel(l_level if l_level is not None else logging.NOTSET)
+        l_fhandler.setFormatter(l_formatter)
 
-    if not quiet:
-        l_shandler = logging.StreamHandler(sys.stdout)
-        l_shandler.setLevel(l_level)
-        l_shandler.setFormatter(l_formatter)
-        l_log.addHandler(l_shandler)
+        # add the handlers to the logger
+        l_log.addHandler(l_fhandler)
+
+    if l_add_qhandler:
+        # create a stdout handler if not set to quiet
+        if not isinstance(quiet, bool):
+            raise KeyError("quiet ({}) is {}, but bool expected.".format(quiet, type(quiet)))
+
+        if not quiet:
+            l_shandler = logging.StreamHandler(sys.stdout)
+            l_shandler.setLevel(l_level)
+            l_shandler.setFormatter(l_formatter)
+            l_log.addHandler(l_shandler)
 
     return l_log
