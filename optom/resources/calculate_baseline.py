90bd0c1afdbec1a46ba0e0429677faaab821a3b6
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2017-03-05 14:54:17 +0100

    calculate baseline for all scenarios

--- optom/resources/calculate_baseline.py
+++ optom/resources/calculate_baseline.py
@@ -4,13 +4,13 @@ import os
 
 f_hdf5 = h5py.File("Scenarios_baseline_time_loss.hdf5", "a")
 
-for i_scenario in ["HE-B49"]:
+for i_scenario in ["NI-B210", "HE-B62", "NW-B1", "HE-B49", "BY-B20", "BY-B471"]:
     for i_stat in ["dissatisfaction", "relative_time_loss", "time_loss"]:
         for i_type in ["alltypes", "passenger", "tractor", "truck"]:
             f_hdf5.create_dataset(
                 name=os.path.join(
                     i_scenario,
-                    "19000.0",
+                    "8640",
                     "best",
                     "global",
                     "driver",
@@ -21,11 +21,11 @@ for i_scenario in ["HE-B49"]:
                     [
                         np.median(
                             np.array(
-                                f_hdf5.get(i_scenario).get("19000.0").get("best").get("global")
+                                f_hdf5.get(i_scenario).get("8640").get("best").get("global")
                                 .get("driver").get(i_type).get("{}_end".format(i_stat))
                             ).T[i]
                         )
-                        for i in xrange(len(f_hdf5.get(i_scenario).get("19000.0").get("best")
+                        for i in xrange(len(f_hdf5.get(i_scenario).get("8640").get("best")
                                             .get("global").get("driver").get(i_type)
                                             .get("{}_end".format(i_stat))[0]))]),
                 compression="gzip",
