84d97ce23c743b45ca8a8827208a6fa8e16b803b
Malte Aschermann <masc@tu-clausthal.de>
2017-03-07 18:54:32 +0100

    license

--- optom/resources/calculate_baseline.py
+++ optom/resources/calculate_baseline.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+# @package optom
+# @cond LICENSE
+# #############################################################################
+# # LGPL License                                                              #
+# #                                                                           #
+# # This file is part of the Optimisation of Overtaking Manoeuvres project.   #
+# # Copyright (c) 2016, Malte Aschermann (malte.aschermann@tu-clausthal.de)   #
+# # This program is free software: you can redistribute it and/or modify      #
+# # it under the terms of the GNU Lesser General Public License as            #
+# # published by the Free Software Foundation, either version 3 of the        #
+# # License, or (at your option) any later version.                           #
+# #                                                                           #
+# # This program is distributed in the hope that it will be useful,           #
+# # but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+# # GNU Lesser General Public License for more details.                       #
+# #                                                                           #
+# # You should have received a copy of the GNU Lesser General Public License  #
+# # along with this program. If not, see http://www.gnu.org/licenses/         #
+# #############################################################################
+# @endcond
+"""Configuration super class."""
+from __future__ import division
+from __future__ import print_function
 import h5py
 import numpy as np
 import os
