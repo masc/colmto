be6ce27f6415456ba37e55e7f5fa701e603478af
Malte Aschermann <malte.aschermann@tu-clausthal.de>
2017-03-10 15:34:00 +0100

    moved io test, adde logger test

---
+++ tests/common/log_test.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# @package tests
+# @cond LICENSE
+# #############################################################################
+# # LGPL License                                                              #
+# #                                                                           #
+# # This file is part of the Optimisation of 2+1 Manoeuvres project.          #
+# # Copyright (c) 2017, Malte Aschermann (malte.aschermann@tu-clausthal.de)   #
+# # This program is free software: you can redistribute it and/or modify      #
+# # it under the terms of the GNU Lesser General Public License as            #
+# # published by the Free Software Foundation, either version 3 of the        #
+# # License, or (at your option) any later version.                           #
+# #                                                                           #
+# # This program is distributed in the hope that it will be useful,           #
+# # but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+# # GNU Lesser General Public License for more details.                       #
+# #                                                                           #
+# # You should have received a copy of the GNU Lesser General Public License  #
+# # along with this program. If not, see http://www.gnu.org/licenses/         #
+# #############################################################################
+# @endcond
+"""
+optom: Test module for optom.common.log.
+"""
+import logging
+
+from nose.tools import assert_equal
+from nose.tools import assert_true
+from nose.tools import assert_raises
+import os
+import tempfile
+
+import optom.common.log
+
+
+def test_logger():
+    """Test logger"""
+    assert_equal(
+        optom.common.log.LOGLEVEL,
+        {
+            "CRITICAL": logging.CRITICAL,
+            "ERROR": logging.ERROR,
+            "WARNING": logging.WARNING,
+            "INFO": logging.INFO,
+            "DEBUG": logging.DEBUG,
+            "NOTSET": logging.NOTSET
+        }
+    )
+    f_temp_log = tempfile.NamedTemporaryFile()
+
+    for i_level in optom.common.log.LOGLEVEL.iterkeys():
+        l_log = optom.common.log.logger(
+            name="foo{}".format(i_level),
+            logfile=f_temp_log.name,
+            quiet=True,
+            loglevel=i_level
+        )
+        assert_true(
+            os.path.exists(os.path.dirname(f_temp_log.name))
+        )
+        assert_true(
+            l_log.name,
+            "foo{}".format(i_level)
+        )
+        assert_equal(
+            l_log.level,
+            optom.common.log.LOGLEVEL.get(i_level)
+        )
+    assert_equal(
+        optom.common.log.logger(
+            name="bar",
+            logfile=f_temp_log.name,
+            quiet=True,
+            loglevel="this should result in NOTSET"
+        ).level,
+        logging.NOTSET
+    )
+
+    with assert_raises(KeyError):
+        optom.common.log.logger(
+            name="bar",
+            logfile=f_temp_log.name,
+            quiet=True,
+            loglevel=["this should fail"]
+        )
+
+    with assert_raises(KeyError):
+        optom.common.log.logger(
+            name="barz",
+            logfile=f_temp_log.name,
+            quiet="foo",
+            loglevel="info"
+        )
